874bba7f49296897f4f8af1b1fa0a40c55cc3f8a
Rachit Nigam <rachit.nigam12@gmail.com>
2020-01-09 13:08:58 -0500

    simply shuffle and add select

--- src/dsp-insts.rkt
+++ src/dsp-insts.rkt
@@ -8,17 +8,11 @@
 ;; Define set of instructions commonly found in DSP architectures.
 
 ;; VECTOR SHUFFLE
-; The implementation differs from the actual instruction in that this allows
-; us to specify the output matrix.
-(define (vector-shuffle inp idxs out)
-  (assert (eq? (vector-length idxs) (vector-length out))
-          "VECTOR-SHUFFLE: mismatch in length of idxs and out")
-  (for ([idx idxs]
-        [out-idx (in-range (vector-length out))])
+(define (vector-shuffle inp idxs)
+  (for/vector ([idx idxs])
     (assert (< idx (vector-length inp))
             "VECTOR-SHUFFLE: idx larger than elements in input vector")
-    (vector-set! out out-idx (vector-ref inp idx)))
-  out)
+    (vector-ref inp idx)))
 
 (define vector-shuffle-tests
   (test-suite
@@ -27,9 +21,8 @@
       "vector shuffle basic example"
       (let ([inp (vector 0 1 2 3 4)]
             [idxs (vector 1 1 0 3 4 2)]
-            [out  (vector 0 0 0 0 0 0)]
             [gold (vector 1 1 0 3 4 2)])
-        (check-equal? gold (vector-shuffle inp idxs out))))
+        (check-equal? gold (vector-shuffle inp idxs))))
 
     (test-case
       "synthesize indices"
@@ -38,12 +31,25 @@
       (define idxs (list->vector idxs-lst))
       (define inp (vector 10 11 22 5 7 14 6))
       (define out-gold (vector 6 22 7 5))
-      (define out (vector 0 0 0 0))
-      (vector-shuffle inp idxs out)
       (define idxs-synth
         (evaluate
           idxs
-          (solve (assert (equal? out out-gold)))))
+          (solve (assert (equal? (vector-shuffle inp idxs) out-gold)))))
       (check-equal? idxs-gold idxs-synth))))
 
+(define (vector-select inp1 inp2 idxs)
+  (vector-shuffle (vector-append inp1 inp2) idxs))
+
+(define vector-select-tests
+  (test-suite
+    "select tests"
+    (test-case
+      "vector select basic example"
+      (let ([inp1 (vector 0 1 2 3)]
+            [inp2 (vector 10 11 12 13)]
+            [idxs (vector 7 7 0 3 2 5)]
+            [gold (vector 13 13 0 3 2 11)])
+        (check-equal? gold (vector-select inp1 inp2 idxs))))))
+
 (run-tests vector-shuffle-tests)
+(run-tests vector-select-tests)
