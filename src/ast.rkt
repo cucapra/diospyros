550d7f319f6dc430c05a9d68c4bdc5595729c37d
Rachit Nigam <rachit.nigam12@gmail.com>
2020-01-14 08:16:49 -0500

    add tests for interp

---
+++ src/ast.rkt
@@ -0,0 +1,61 @@
+#lang rosette
+
+(provide (all-defined-out))
+
+; XXX(rachit): Every instruction has an `id` which seems bad.
+; A program is a sequence of instructions.
+(struct prog (insts) #:transparent)
+
+; Load and unload vectors from memory.
+(struct vec-load (id start size) #:transparent)
+(struct vec-unload (id start) #:transparent)
+
+; Set constant vector in memory.
+(struct vec-const (id init) #:transparent)
+
+; Shuffle instructions inside memory.
+(struct vec-shuffle (id inp idxs) #:transparent)
+(struct vec-select (id inp1 inp2 idxs) #:transparent)
+(struct vec-shuffle-set! (out-vec inp idxs) #:transparent)
+
+; Apply functions on vectors.
+(struct vec-app (id f inps) #:transparent)
+
+(require (for-syntax racket/base
+                     syntax/parse)
+         racket/syntax
+         racket/stxparam)
+
+(define-syntax (define/prog stx)
+  (define-syntax-class instruction
+    ; #:description "instruction specification"
+    #:datum-literals (=)
+    (pattern
+      (id:expr = inst:id param:expr ...)
+      #:with to-inst #'(inst id param ...))
+    (pattern
+      (inst:id param:expr ...)
+      #:with to-inst #'(inst param ...)))
+
+  (syntax-parse stx
+    ; Parse functions that return values
+    [(_ name:id inst:instruction ...)
+     #'(define name (prog (list inst.to-inst ...)))]))
+
+(module+ test
+  (require rackunit
+           rackunit/text-ui)
+  (run-tests
+    (test-suite
+      "AST tests"
+      (test-case
+        "syntax macro generates a valid program"
+        (define/prog p
+          ('x = vec-load 10 20)
+          (vec-unload 'x 20))
+        (define prog-gold
+          (prog
+            `(
+              ,(vec-load 'x 10 20)
+              ,(vec-unload 'x 20))))
+        (check-equal? p prog-gold)))))
--- src/interp.rkt
+++ src/interp.rkt
@@ -6,6 +6,16 @@
 
 (provide (all-defined-out))
 
+; Read a vector of `size` starting at location `start` from `memory`
+(define (read-vec memory start size)
+  (vector-copy memory start (+ start size)))
+
+; Write a vector to `memory` to the location `start` and return the updated
+; memory.
+; MUTATES the argument `memory`.
+(define (write-vec! memory start vec)
+  (vector-copy! memory start vec))
+
 ; Interpretation function that takes a program and an external memory.
 ; Returns a vector representing the state of the final memory.
 (define (interp program memory)
@@ -17,12 +27,10 @@
 
   (for ([inst (prog-insts program)])
     (match inst
-      [(vec-load id start end)
-       (env-set! id (vector-copy memory
-                                 start
-                                 end))]
+      [(vec-load id start size)
+       (env-set! id (read-vec memory start size))]
       [(vec-unload id start)
-       (vector-copy! memory start (env-ref id))]
+       (write-vec! memory start (env-ref id))]
       [(vec-const id init)
        (env-set! id init)]
       [(vec-shuffle id inp idxs)
@@ -43,3 +51,37 @@
        (env-set! id (apply f (map env-ref inps)))]))
 
   memory)
+
+(module+ test
+  (require rackunit
+           rackunit/text-ui)
+  (run-tests
+    (test-suite
+      "interp tests"
+      (test-case
+        "Write out constant vector"
+        (define memory
+          (make-vector 4 0))
+        (define gold
+          (vector 1 2 3 4))
+        (define/prog p
+          ('x = vec-const gold)
+          (vec-unload 'x 0))
+        (interp p memory)
+        (check-equal? (read-vec memory 0 4) gold))
+
+      (test-case
+        "create shuffled vector"
+        (define memory
+          (make-vector 4 0))
+        (define gold
+          (vector 3 1 2 0))
+        (define/prog p
+          ('const = vec-const (vector 0 1 2 3))
+          ('idxs = vec-const (vector 3 1 2 0))
+          ('shuf = vec-shuffle 'const 'idxs)
+          (vec-unload 'shuf 0))
+        (interp p memory)
+        (check-equal? (read-vec memory 0 4) gold))
+
+      )))
