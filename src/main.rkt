8a2a1f0b6de333c295c1c6ae4fda72c8765e5e40
Alexa VanHattum <amv94@cornell.edu>
2020-02-17 11:23:01 -0500

    Update main to eval program in the right namespace

--- src/main.rkt
+++ src/main.rkt
@@ -1,6 +1,7 @@
 #lang rosette
 
 (require "compiler.rkt"
+         "ast.rkt"
          "c-ast.rkt"
          "backend/tensilica-g3.rkt"
          threading
@@ -19,13 +20,20 @@
       #:args (filename)
       filename))
 
+  (define input-string (read (open-input-file file-to-compile)))
+
+  ; To eval the input, use the current namespace
+  (define-namespace-anchor a)
+  (define ns (namespace-anchor->namespace a))
+  (define input-program (eval input-string ns))
+
   (when (not (out-file))
     (error 'dios
            "Missing output file."))
 
   (call-with-output-file (build-path (current-directory) (out-file))
     (lambda (out)
-      (~> file-to-compile
+      (~> input-program
           compile
           tensilica-g3-compile
           to-string
