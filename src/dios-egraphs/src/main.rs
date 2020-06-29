4a9d76efe4b0c09197471698cc177590e720de80
Alexa VanHattum <amv94@cornell.edu>
2020-06-29 11:34:07 -0400

    2x2 3x3 matmul in egg

--- src/dios-egraphs/src/main.rs
+++ src/dios-egraphs/src/main.rs
@@ -19,6 +19,7 @@ use egg::{rewrite as rw, *};
 define_language! {
     enum VecLang {
         Num(i32),
+
         // Id is a key to identify EClasses within an EGraph, represents
         // children nodes
         "+" = Add(Box<[Id]>),
@@ -27,16 +28,59 @@ define_language! {
         // Lists have a variable number of elements
         "List" = List(Box<[Id]>),
 
-        // For now, vectors always have 2 lanes
-        "VecAdd" = VecAdd(Box<[Id]>),
-        "VecMul" = VecMul(Box<[Id]>),
+        // Vectors have 4 elements
+        "Vec4" = Vec4([Id; 4]),
+
+        // Used for partitioning and recombining lists
+        "Concat" = Concat([Id; 2]),
+
+        // Vector operations that take 2 vectors of inputs
+        "VecAdd" = VecAdd([Id; 2]),
+        "VecMul" = VecMul([Id; 2]),
+
+        // MAC takes 3 lists: acc, v1, v2
         "VecMAC" = VecMAC([Id; 3]),
+
         // language items are parsed in order, and we want symbol to
         // be a fallback, so we put it last.
         // `Symbol` is an egg-provided interned string type
         Symbol(egg::Symbol),
     }
 }
+
+type EGraph = egg::EGraph<VecLang, VecLangAnalysis>;
+
+#[derive(Default)]
+struct VecLangAnalysis;
+
+#[derive(Debug)]
+struct Data {
+}
+
+impl Analysis<VecLang> for VecLangAnalysis {
+    type Data = Data;
+    fn merge(&self, _to: &mut Data, _from: Data) -> bool {
+        false
+    }
+
+    fn make(_egraph: &EGraph, _enode: &VecLang) -> Data {
+        Data { }
+    }
+}
+
+struct ListApplier(&'static str);
+impl Applier<VecLang, VecLangAnalysis> for ListApplier {
+    fn apply_one(&self, _: &mut EGraph, _: Id, _: &Subst) -> Vec<Id> {
+        panic!()
+    }
+}
+
+// // This returns a function that implements Condition
+// fn is_not_zero(var: &'static str) -> impl Fn(&mut EGraph, Id, &Subst) -> bool {
+//     let var = var.parse().unwrap();
+//     let zero = VecLang::Num(0);
+//     move |egraph, _, subst| !egraph[subst[var]].nodes.contains(&zero)
+// }
 
 struct VecCostFn;
 impl CostFunction<VecLang> for VecCostFn {
@@ -73,6 +117,19 @@ fn main() {
         rw!("add-0"; "(+ ?a 0)" => "?a"),
         rw!("mul-0"; "(* ?a 0)" => "0"),
         rw!("mul-1"; "(* ?a 1)" => "?a"),
+
+        // Partition lists - hardcoded
+        rw!("partition-5"; "(List ?a ?b ?c ?d ?e)"
+            => "(Concat (List ?a ?b ?c ?d) (List ?e))"),
+        rw!("partition-6"; "(List ?a ?b ?c ?d ?e ?f)"
+            => "(Concat (List ?a ?b ?c ?d) (List ?e ?f))"),
+        rw!("partition-7"; "(List ?a ?b ?c ?d ?e ?f ?g)"
+            => "(Concat (List ?a ?b ?c ?d) (List ?e ?f ?g))"),
+        rw!("partition-8"; "(List ?a ?b ?c ?d ?e ?f ?g ?h)"
+            => "(Concat (List ?a ?b ?c ?d) (List ?e ?f ?g ?h))"),
+
+        rw!("partition-9"; "(List ?v0 ?v1 ?v2 ?v3 ?v4 ?v5 ?v6 ?v7 ?v8)"
+            => "(Concat (List ?v0 ?v1 ?v2 ?v3) (List ?v4 ?v5 ?v6 ?v7 ?v8))"),
 
         // For now, hardcode vectors with 2-4 lanes
         rw!("vec-add-2"; "(List (+ ?a ?b) (+ ?c ?d))"
@@ -254,4 +311,86 @@ fn main() {
     //  (box (bvadd (bvmul v$3 v$6) (bvmul v$4 v$9) (bvmul v$5 v$12)))
     //  (box (bvadd (bvmul v$3 v$7) (bvmul v$4 v$10) (bvmul v$5 v$13)))
     //  (box (bvadd (bvmul v$3 v$8) (bvmul v$4 v$11) (bvmul v$5 v$14))))
+    {
+        let start = "(List
+                        (+ (* v0 v6) (* v1 v9)  (* v2 v12))
+                        (+ (* v0 v7) (* v1 v10) (* v2 v13))
+                        (+ (* v0 v8) (* v1 v11) (* v2 v14))
+                        (+ (* v3 v6) (* v4 v9)  (* v5 v12))
+                        (+ (* v3 v7) (* v4 v10) (* v5 v13))
+                        (+ (* v3 v8) (* v4 v11) (* v5 v14)))"
+            .parse()
+            .unwrap();
+        let runner = Runner::default().with_expr(&start).run(&rules);
+        println!(
+            "Stopped after {} iterations, reason: {:?}",
+            runner.iterations.len(),
+            runner.stop_reason
+        );
+
+        let (egraph, root) = (runner.egraph, runner.roots[0]);
+
+        let mut extractor = Extractor::new(&egraph, VecCostFn);
+        let (best_cost, best) = extractor.find_best(root);
+        println!(
+            "original:\n{}\nbest:\n{}\nbest cost {}",
+            start.pretty(40),
+            best.pretty(40),
+            best_cost,
+        );
+        assert_eq!(
+            best,
+            "(Concat
+              (VecMAC
+                (VecMAC
+                  (VecMul
+                    (List v2 v2 v2 v12)
+                    (List v12 v13 v14 v5))
+                  (List v1 v1 v1 v9)
+                  (List v9 v10 v11 v4))
+                (List v0 v0 v0 v6)
+                (List v6 v7 v8 v3))
+              (VecMAC
+                (VecMAC
+                  (VecMul (List v13 v14) (List v5 v5))
+                  (List v10 v11)
+                  (List v4 v4))
+                (List v7 v8)
+                (List v3 v3)))"
+                .parse()
+                .unwrap()
+        );
+    }
+    {
+        let start = "(List
+                         (* v0 v4)
+                         (+ (* v0 v5) (* v1 v4))
+                         (* v1 v5)
+                         (+ (* v0 v6) (* v2 v4))
+                         (+ (* v0 v7) (* v1 v6) (* v2 v5) (* v3 v4))
+                         (+ (* v1 v7) (* v3 v5))
+                         (* v2 v6)
+                         (+ (* v2 v7) (* v3 v6))
+                         (* v3 v7))"
+            .parse()
+            .unwrap();
+        let runner = Runner::default().with_expr(&start).run(&rules);
+        println!(
+            "Stopped after {} iterations, reason: {:?}",
+            runner.iterations.len(),
+            runner.stop_reason
+        );
+
+        let (egraph, root) = (runner.egraph, runner.roots[0]);
+
+        let mut extractor = Extractor::new(&egraph, VecCostFn);
+        let (best_cost, best) = extractor.find_best(root);
+        println!(
+            "original:\n{}\nbest:\n{}\nbest cost {}",
+            start.pretty(40),
+            best.pretty(40),
+            best_cost,
+        );
+    }
+
     }
