4edf375fe4d45bbceb20afcdd3fba1d3f8a9cb01
Alexa VanHattum <amv94@cornell.edu>
2020-07-02 17:11:31 -0400

    Update cost model to prioritize vectors with all literal children

--- src/dios-egraphs/src/main.rs
+++ src/dios-egraphs/src/main.rs
@@ -93,23 +93,32 @@ impl CostFunction<VecLang> for VecCostFn {
     where
         C: FnMut(Id) -> Self::Cost,
     {
+        const BIG: f64 = 100.0;
+        const LITERAL: f64 = 0.001;
+        const STRUCTURE: f64 = 0.1;
+        const OP: f64 = 1.;
         let op_cost = match enode {
             // You get literals for extremely cheap
-            VecLang::Num(..) => 0.01,
-            VecLang::Symbol(..) => 0.01,
+            VecLang::Num(..) => LITERAL,
+            VecLang::Symbol(..) => LITERAL,
 
-            // And list/vector structures for quite cheap
-            VecLang::List(..) => 0.1,
-            VecLang::Vec4(..) => 0.1,
-            VecLang::Concat(..) => 0.1,
+            // And list structures for quite cheap
+            VecLang::List(..) => STRUCTURE,
+            VecLang::Concat(..) => STRUCTURE,
+
+            // Vectors are cheap if they have literal values
+            VecLang::Vec4(vals) => {
+                let non_literals = vals.iter().any(|&x| costs(x) > LITERAL);
+                if non_literals {BIG} else {STRUCTURE}
+            },
 
             // But scalar and vector ops cost something
-            VecLang::Add(..) => 1.,
-            VecLang::Mul(..) => 1.,
+            VecLang::Add(..) => OP,
+            VecLang::Mul(..) => OP,
 
-            VecLang::VecAdd(..) => 1.,
-            VecLang::VecMul(..) => 1.,
-            VecLang::VecMAC(..) => 1.,
+            VecLang::VecAdd(..) => OP,
+            VecLang::VecMul(..) => OP,
+            VecLang::VecMAC(..) => OP,
         };
         enode.fold(op_cost, |sum, id| sum + costs(id))
     }
@@ -438,22 +447,6 @@ fn main() {
             best_cost,
         );
     }
-
-
-        // let start = "(Vec4
-        //                 (+ (* v0 v7) (* v1 v6) (* v2 v5) (* v3 v4))
-        //                 (+ (* v1 v7) (* v3 v5))
-        //                 (* v2 v6)
-        //                 (+ (* v2 v7) (* v3 v6)))"
-
-    // THIS ONE WORKS
-        // let start = "(Vec4
-        //                 (+ (* v0 v7) (+ (* v1 v6) (+ (* v2 v5) (* v3 v4))))
-        //                 (+ (* v1 v7) (+ (* v3 v5) (+ (* 0 0) (* 0 0))))
-        //                 (+ (* v2 v6) (+ (* 0 0) (+ (* 0 0) (* 0 0))))
-        //                 (+ (* v2 v7) (+ (* v3 v6) (+ (* 0 0) (* 0 0)))))"
-
-    // This one finds cost = 4
     // Pattern search example
     {
         let start = "(Vec4
@@ -552,5 +545,99 @@ fn main() {
         assert_eq!(egraph2.add_expr(&start_with_zero), egraph2.add_expr(&goal));
         assert_eq!(egraph2.add_expr(&start_simplified), egraph2.add_expr(&goal));
     }
+    {
+        let start = "(Concat
+          (Vec4
+            (* v0 v9)
+            (+ (* v0 v10) (* v1 v9))
+            (+ (* v0 v11) (* v1 v10) (* v2 v9))
+            (+ (* v1 v11) (* v2 v10)))
+          (Concat
+            (Vec4
+              (* v2 v11)
+              (+ (* v0 v12) (* v3 v9))
+              (+ (* v0 v13) (* v1 v12) (* v3 v10) (* v4 v9))
+              (+ (* v0 v14)
+                 (* v1 v13)
+                 (* v2 v12)
+                 (* v3 v11)
+                 (* v4 v10)
+                 (* v5 v9)))
+            (Concat
+              (Vec4
+                (+ (* v1 v14)
+                   (* v2 v13)
+                   (* v4 v11)
+                   (* v5 v10))
+                (+ (* v2 v14) (* v5 v11))
+                (+ (* v0 v15) (* v3 v12) (* v6 v9))
+                (+ (* v0 v16)
+                   (* v1 v15)
+                   (* v3 v13)
+                   (* v4 v12)
+                   (* v6 v10)
+                   (* v7 v9)))
+              (Concat
+                (Vec4
+                  (+ (* v0 v17)
+                     (* v1 v16)
+                     (* v2 v15)
+                     (* v3 v14)
+                     (* v4 v13)
+                     (* v5 v12)
+                     (* v6 v11)
+                     (* v7 v10)
+                     (* v8 v9))
+                  (+ (* v1 v17)
+                     (* v2 v16)
+                     (* v4 v14)
+                     (* v5 v13)
+                     (* v7 v11)
+                     (* v8 v10))
+                  (+ (* v2 v17) (* v5 v14) (* v8 v11))
+                  (+ (* v3 v15) (* v6 v12)))
+                (Concat
+                  (Vec4
+                    (+ (* v3 v16) (* v4 v15) (* v6 v13) (* v7 v12))
+                    (+ (* v3 v17)
+                       (* v4 v16)
+                       (* v5 v15)
+                       (* v6 v14)
+                       (* v7 v13)
+                       (* v8 v12))
+                    (+ (* v4 v17) (* v5 v16) (* v7 v14) (* v8 v13))
+                    (+ (* v5 v17) (* v8 v14)))
+                  (Concat
+                    (Vec4
+                      (* v6 v15)
+                      (+ (* v6 v16) (* v7 v15))
+                      (+ (* v6 v17) (* v7 v16) (* v8 v15))
+                      (+ (* v7 v17) (* v8 v16)))
+                    (List
+                      (* v8 v17))))))))"
+            .parse()
+            .unwrap();
+        let runner = Runner::default()
+            .with_expr(&start)
+            .with_node_limit(900_000)
+            .with_time_limit(std::time::Duration::from_secs(120))
+            .with_iter_limit(60)
+            .run(&rules);
+        println!(
+            "Stopped after {} iterations, reason: {:?}",
+            runner.iterations.len(),
+            runner.stop_reason
+        );
 
+        let (egraph, root) = (runner.egraph, runner.roots[0]);
+
+        let mut extractor = Extractor::new(&egraph, VecCostFn);
+        let (best_cost, best) = extractor.find_best(root);
+        println!(
+            "original:\n{}\nbest:\n{}\nbest cost {}",
+            start.pretty(40),
+            best.pretty(40),
+            best_cost,
+        );
     }
+}
