11918802de19710733d1e6471dc969e81e72981a
Alexa VanHattum <amv94@cornell.edu>
2020-07-25 11:00:18 -0400

    Search for macs with a single add mul pattern, subst renaming

--- src/dios-egraphs/src/macsearcher.rs
+++ src/dios-egraphs/src/macsearcher.rs
@@ -1,9 +1,14 @@
 use egg::{*};
 
+use std::str::FromStr;
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct MacSearcher<L> {
     pub full_mac_pattern: Pattern<L>,
     pub vec4_pattern: Pattern<L>,
+    pub add_mul_pattern: Pattern<L>,
+    pub mul_pattern: Pattern<L>,
+    pub zero_pattern: Pattern<L>,
 }
 
 impl<L: Language> MacSearcher<L> {
@@ -20,10 +25,10 @@ impl<L: Language> MacSearcher<L> {
 //       (+ ?g (* ?h ?i))
 //       (+ ?j (* ?k ?l)))
 pub fn build_mac_searcher<L: Language>() -> MacSearcher<L> {
-    let full_mac_pattern : Pattern<L> = "(Vec4 (+ ?a (* ?b ?c))
-                                               (+ ?d (* ?e ?f))
-                                               (+ ?g (* ?h ?i))
-                                               (+ ?j (* ?k ?l)))"
+    let full_mac_pattern : Pattern<L> = "(Vec4 (+ ?a0 (* ?b0 ?c0))
+                                               (+ ?a1 (* ?b1 ?c1))
+                                               (+ ?a2 (* ?b2 ?c2))
+                                               (+ ?a3 (* ?b3 ?c3)))"
         .parse::<Pattern<L>>()
         .unwrap();
 
@@ -31,9 +36,24 @@ pub fn build_mac_searcher<L: Language>() -> MacSearcher<L> {
         .parse::<Pattern<L>>()
         .unwrap();
 
+    let add_mul_pattern : Pattern<L> = "(+ ?a (* ?b ?c))"
+        .parse::<Pattern<L>>()
+        .unwrap();
+
+    let mul_pattern : Pattern<L> = "(* ?b ?c)"
+        .parse::<Pattern<L>>()
+        .unwrap();
+
+    let zero_pattern : Pattern<L> = "0"
+        .parse::<Pattern<L>>()
+        .unwrap();
+
     MacSearcher {
         full_mac_pattern,
         vec4_pattern,
+        add_mul_pattern,
+        mul_pattern,
+        zero_pattern,
     }
 }
 
@@ -51,36 +71,47 @@ impl<L: Language, A: Analysis<L>> Searcher<L, A> for MacSearcher<L> {
                 // 0
                 let mut new_substs : Vec<Subst> = Vec::new();
 
+                // TODO: do we need to do combinatorial madness across the
+                // vector lanes?
+
                 // For each set of substitutions
                 for substs in matches.substs.iter() {
-                    let new_sub = Subst::with_capacity(12);
-                    let mut all_matches_found = false;
+                    let mut new_sub = Subst::with_capacity(12);
+                    let mut all_matches_found = true;
                     // For each variable x, y, x, w in (Vec4 ?x ?y ?z ?w)
-                    for vec4_var in self.vec4_pattern.vars().iter() {
+                    for (i, vec4_var) in self.vec4_pattern.vars().iter().enumerate() {
+
                         // Check if that variable matches add/mul
-                        let add_mul_pattern : Pattern<L> = "(+ ?a (* ?b ?c))"
-                            .parse::<Pattern<L>>()
-                            .unwrap();
-                        if let Some(child_eclass) = substs.get(*vec4_var) {
-                            // println!("{:?}", *child_eclass);
-                            if let Some(add_mul_match) = add_mul_pattern.search_eclass(egraph, *child_eclass) {
-                                println!("{:?}", add_mul_match);
-                                // TODO: add all matches found to new_sub, with
-                                // renaming
-                                continue;
+                        let child_eclass = substs.get(*vec4_var).unwrap();
+                        if let Some(add_mul_match) = self.add_mul_pattern.search_eclass(egraph, *child_eclass) {
+                            for s in add_mul_match.substs.iter() {
+                                for add_mul_var in self.add_mul_pattern.vars().iter() {
+                                    let vs : String = format!("{}", *add_mul_var);
+                                    let new_v = Var::from_str(&format!("{}{}", vs, i)).unwrap(); // Add post fix!!!
+                                    new_sub.insert(new_v, *s.get(*add_mul_var).unwrap());
+                                }
                             }
+                        } else {
+                            all_matches_found = false;
+                            break;
                         }
-                        all_matches_found = false;
-                        break;
                     }
                     if all_matches_found {
                         new_substs.push(new_sub);
                     }
                 };
-                Some(matches)
+                if new_substs.is_empty() {
+                    None
+                } else {
+                    Some(SearchMatches {
+                        eclass : matches.eclass,
+                        substs : new_substs,
+                    })
+                }
             }
         };
-        self.full_mac_pattern.search_eclass(egraph, eclass)
+        // self.full_mac_pattern.search_eclass(egraph, eclass)
+        vec4_mac_compatible
     }
 
     fn vars(&self) -> Vec<Var> {
