8ce0895b10379f53a941222c75ee71826b8f653a
Alexa VanHattum <amv94@cornell.edu>
2020-08-21 11:53:21 -0400

    Ablation chart style nits

--- evaluation/ablation.py
+++ evaluation/ablation.py
@@ -20,21 +20,25 @@ def plot(df):
     """
     Generates an ablation plot using the given data frame.
     """
-    # Nature has a 0 timeout because we want it to be at the top.
+    # Nature has a 10000 timeout because we want it to be at the bottom.
     df['timeout'] = df['name'].map(
-        lambda x: 0 if x == 'nature' else int(x.split('-')[1]))
+        lambda x: 1000 if x == 'Nature' else int(x.split('-')[1]))
     # Cleanup the kernel names
     df['name'] = df['name'].map(
-        lambda x: x if x == 'nature' else x.split('-')[1])
-    # Color palette hackery
-    pal = df['name'].map(
-        lambda x: colorblind[4] if x == 'nature' else colorblind[2])
+        lambda x: 'Nature' if x == 'Nature' else x.split('-')[1])
+
+    plt.rcParams['figure.figsize'] = (10, 3)
 
     print(df)
     df = df.sort_values(['timeout'])
 
+    # Color palette hackery
+    pal = df['name'].map(
+        lambda x: colorblind[4] if x == 'Nature' else colorblind[2])
+
     # Set SNS formatting
-    sns.set(font_scale=1.07)
+    sns.set(font_scale=1.6)
+
 
     ax = sns.barplot(
         y='name',
@@ -44,9 +48,8 @@ def plot(df):
     )
     locs, labels = plt.xticks()
 
-    ax.legend(loc=1)
     ax.set_ylabel('Timeout (seconds)')
-    ax.set_xlabel('Simulation cycles')
+    ax.set_xlabel('Simulation cycles, 10×10 10×10 MatMul')
     plt.savefig('ablation.pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
