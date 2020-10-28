12b56d37ca0d2d6fb084e12857e928cd98cbbcfa
Rachit Nigam <rachit.nigam12@gmail.com>
2020-10-28 15:28:27 -0400

    to_egg seems to work

--- vec-dsl-conv/src/main.rs
+++ vec-dsl-conv/src/main.rs
@@ -4,17 +4,17 @@ use std::io::{self, Read};
 
 /// Transforms a named location in an array to a Get expressions.
 /// For example, the name A$13 is transformed into (Get A 13).
-fn to_value(v: lexpr::Value, erase: bool) -> lexpr::Value {
-    if let lexpr::Value::String(s) = v {
-        let mut split = s.split("$").collect::<Vec<_>>();
-        let (pre, idx) = (split.remove(0), split.remove(1));
-        if !erase {
-            lexpr::Value::list(vec!["Get", pre, idx])
-        } else {
-            pre.into()
-        }
+fn to_value(s: &str, erase: bool) -> lexpr::Value {
+    let mut split = s.split("$").collect::<Vec<_>>();
+    let (pre, idx) = (split.remove(0), split.remove(0));
+    if !erase {
+        lexpr::Value::list(vec![
+            lexpr::Value::symbol("Get"),
+            lexpr::Value::symbol(pre),
+            lexpr::Value::Number(idx.parse::<u64>().unwrap().into()),
+        ])
     } else {
-        panic!("Passed a non-string value to to_value");
+        lexpr::Value::Symbol(pre.into())
     }
 }
 
@@ -23,9 +23,9 @@ fn to_value(v: lexpr::Value, erase: bool) -> lexpr::Value {
 fn to_egg(expr: lexpr::Value, erase: bool, rewrites: &HashMap<&str, &str>) -> lexpr::Value {
     match expr {
         lexpr::Value::Number(_) => expr,
-        lexpr::Value::String(_) => to_value(expr, erase),
+        lexpr::Value::Symbol(s) => to_value(&*s, erase),
         lexpr::Value::Cons(c) => {
-            if let (lexpr::Value::String(head), lexpr::Value::Cons(tail)) = c.into_pair() {
+            if let (lexpr::Value::Symbol(head), lexpr::Value::Cons(tail)) = c.into_pair() {
                 if &*head == "box" {
                     let (mut t, _) = tail.into_vec();
                     assert!(t.len() == 1, "Boxed value had more than one element");
@@ -33,6 +33,8 @@ fn to_egg(expr: lexpr::Value, erase: bool, rewrites: &HashMap<&str, &str>) -> le
                 } else if &*head == "app" {
                     return to_egg(tail.into(), erase, rewrites);
                 } else {
+                    // The operator name might need to change
+                    let op = lexpr::Value::symbol(rewrites.get(&*head).unwrap_or(&&*head).clone());
                     // Early return if this is a simple list
                     let mut children = tail
                         .into_vec()
@@ -41,23 +43,22 @@ fn to_egg(expr: lexpr::Value, erase: bool, rewrites: &HashMap<&str, &str>) -> le
                         .map(|v| to_egg(v, erase, rewrites))
                         .collect_vec();
                     if &*head == "list" && children.len() < 3 {
-                        return lexpr::Value::cons(head, lexpr::Value::from(children));
+                        let mut list = vec![op];
+                        list.append(&mut children);
+                        return lexpr::Value::list(list);
                     }
-
-                    // The operator name might need to change
-                    let op = rewrites.get(&*head).unwrap_or(&&*head).clone();
                     // Rewrite all the children in the list.
                     children.reverse();
+                    let init = children.remove(0);
                     children
                         .into_iter()
-                        .fold1(|x, y| lexpr::Value::cons(op, lexpr::Value::cons(x, y)))
-                        .unwrap()
+                        .fold(init, |acc, x| lexpr::Value::list(vec![op.clone(), x, acc]))
                 }
             } else {
                 panic!("Head of a list was not a string")
             }
-        },
-        _ => panic!("Unexpected lexpr value")
+        }
+        v => panic!("Unexpected lexpr value: {}", v),
     }
 }
 
@@ -67,6 +68,9 @@ fn main() -> io::Result<()> {
     io::stdin().read_to_string(&mut buffer)?;
     // Parse the given S-expr
     let v = lexpr::from_str(&buffer)?;
-    println!("{}", v.to_string());
+    // Rewrite specifications
+    let mut rewrites = HashMap::new();
+    rewrites.insert("list", "List");
+    println!("{}", to_egg(v, false, &rewrites));
     Ok(())
 }
