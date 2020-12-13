0f0718c7150e0c3618d87b3ad2a832ec55d05f04
Adrian Sampson <adrian@radbox.org>
2020-12-13 16:16:37 -0500

    Add plain-text output

--- evaluation/benchtbl.py
+++ evaluation/benchtbl.py
@@ -38,6 +38,7 @@ def split_size(size_str):
     name = "".join([s for s in str_rows if not is_size(s)])
     return size, name
 
+
 # Turn a size string into a single int for comparison within a benchmark
 def compare_size(size_str):
     def array_to_size(a):
@@ -55,7 +56,11 @@ def compare_size(size_str):
         combined_size += size * pow(10, i * 2)
     return combined_size
 
-def benchtbl():
+
+def benchtbl(plain=False):
+    """Dump a table. Make a LaTeX table by default. Otherwise, produce
+    plain text suitable for viewing in a terminal.
+    """
     # Descriptions of the benchmarks.
     with open('benchmarks.csv') as f:
         reader = csv.DictReader(f)
@@ -92,7 +97,7 @@ def benchtbl():
                         stime_txt = '{:.1f}s'.format(stime)
 
                     if row['Saturated?'] != "Yes":
-                        stime_txt += "\\rlap{$^\dagger$}"
+                        stime_txt += '*' if plain else "\\rlap{$^\dagger$}"
                 else:
                     stime_txt = ''
 
@@ -116,22 +121,31 @@ def benchtbl():
             v.sort(key=lambda v: compare_size(v['size']))
 
 
-    print(r'\begin{tabular}{llrcrrc}')
-    print(r'\toprule')
-    print_row(wrap_cmd('textbf', s) for s in COLS)
-    print(r'\midrule')
-    for j, row in enumerate(benchmarks):
-        # Separation between benchmarks.
-        if j != 0:
-            print(r'\addlinespace[.7em]')
+    if plain:
+        rowfmt = '{:<10} {:<10} {:<15} {:<10} {:<10}'
+        cols = [c.replace('\\', '') for c in COLS if c != 'Description']
+        print(rowfmt.format(*cols))
+        print(rowfmt.format(*('=' * len(c) for c in cols)))
+    else:
+        print(r'\begin{tabular}{llrcrrc}')
+        print(r'\toprule')
+        print_row(wrap_cmd('textbf', s) for s in COLS)
+        print(r'\midrule')
 
+    for j, row in enumerate(benchmarks):
         sizes = benchdata[row['bench']]
-        multirow = r'multirow[t]{{{}}}{{*}}'.format(len(sizes) or 1)
-        print_row([wrap_cmd(multirow, s) for s in [
-            wrap_cmd('bench', row['bench']),
-            row['desc'],
-            row['ref-loc'],
-        ]], end=False)
+
+        if not plain:
+            # Separation between benchmarks.
+            if j != 0:
+                print(r'\addlinespace[.7em]')
+
+            multirow = r'multirow[t]{{{}}}{{*}}'.format(len(sizes) or 1)
+            print_row([wrap_cmd(multirow, s) for s in [
+                wrap_cmd('bench', row['bench']),
+                row['desc'],
+                row['ref-loc'],
+            ]], end=False)
 
         if not sizes:
             print('no results for {}'.format(row['bench']), file=sys.stderr)
@@ -139,18 +153,31 @@ def benchtbl():
             continue
 
         for i, size in enumerate(sizes):
-            if i != 0:
-                # On non-first sizes, pad out the left columns.
-                print('&' * 3, end=' ')
-            print_row([
-                size['size'],
-                size['time'],
-                size['memory'],
-            ])
-    print(r'\bottomrule')
-    print(r'\end{tabular}')
-    print(r'\\$\dagger$ Equality saturation timed out after 180s.')
+            if plain:
+                print(rowfmt.format(
+                    row['bench'] if i == 0 else '',
+                    row['ref-loc'] if i == 0 else '',
+                    size['size'],
+                    size['time'],
+                    size['memory'].replace('~', ' '),
+                ))
+            else:
+                if i != 0:
+                    # On non-first sizes, pad out the left columns.
+                    print('&' * 3, end=' ')
+                print_row([
+                    size['size'],
+                    size['time'],
+                    size['memory'],
+                ])
+
+    if plain:
+        print('* Equality saturation timed out after 180s.')
+    else:
+        print(r'\bottomrule')
+        print(r'\end{tabular}')
+        print(r'\\$\dagger$ Equality saturation timed out after 180s.')
 
 
 if __name__ == '__main__':
-    benchtbl()
+    benchtbl('--plain' in sys.argv[1:])
