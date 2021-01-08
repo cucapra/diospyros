363be7823628653f26debd2713f17ac5e2d147f5
Alexa VanHattum <amv94@cornell.edu>
2021-01-08 09:05:28 -0500

    Workaround for cargo fmt bug

--- src/dios-egraphs/src/config.rs
+++ src/dios-egraphs/src/config.rs
@@ -2,15 +2,15 @@
 pub const fn vector_width() -> usize {
     #[cfg(feature = "vec_width_2")]
     {
-{        2
+        2
     }
     #[cfg(feature = "vec_width_8")]
     {
-{        8
+        8
     }
     // Default to a width of 4
     #[cfg(not(any(feature = "vec_width_2", feature = "vec_width_8")))]
     {
-{        4
+        4
     }
 }
