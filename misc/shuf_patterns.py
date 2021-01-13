4daca0b6b92b613777232dff5639bd547edb1db8
Adrian Sampson <adrian@radbox.org>
2021-01-13 14:34:08 -0500

    Docstrings

--- misc/shuf_patterns.py
+++ misc/shuf_patterns.py
@@ -1,3 +1,9 @@
+"""Extract immediate shuffle patterns from Xtensa documentation.
+
+Given an HTML file from the documentation for an Xtensa `SHFLI` or
+`SELI` instruction, emit information about the available patterns for a
+given bit width (currently 32).
+"""
 import sys
 import re
 
@@ -7,6 +13,13 @@ BIT_WIDTH = 32
 
 
 def get_immediates(infile):
+    """Parse an HTML documentation file and emit patterns.
+
+    For each pattern, produce the index (the underlying "magic number"
+    for the pattern), the name (the C constant in the header file), and
+    a list of byte indices (indicating the exact byte-level immediate
+    shuffle pattern).
+    """
     for line in infile:
         m = re.match(IMMEDIATE_PATTERN, line)
         if m:
@@ -16,6 +29,12 @@ def get_immediates(infile):
 
 
 def coarsen_indices(byte_indices, size):
+    """Convert a byte-level shuffle pattern to a coarser one.
+
+    Given `size`, which is a number of bytes, convert an `N`-length
+    shuffle pattern list to an `N/size`-length one that describes the
+    pattern at the level of those "words."
+    """
     for i in range(0, len(byte_indices), size):
         byte_chunk = byte_indices[i:i + size]
         assert byte_chunk[0] % size == 0
@@ -26,6 +45,8 @@ def coarsen_indices(byte_indices, size):
 
 
 def extract(infile):
+    """Extract all the patterns from the documentation and print.
+    """
     for idx, name, byte_indices in get_immediates(infile):
         if '{}B'.format(BIT_WIDTH) in name:
             word_indices = list(coarsen_indices(byte_indices, BIT_WIDTH // 8))
