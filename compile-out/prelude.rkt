fbc99319d3d66ada3eab517342cfcbeea025ae5e
Jonathan Tran <jtran2718@gmail.com>
2021-03-04 15:11:32 -0500

    remove compile out

--- compile-out/prelude.rkt
+++
@@ -1,6 +0,0 @@
-(prog
- (list
-  (vec-const 'Z '#(0) 'float)
-  (vec-extern-decl 'q_in 4 'extern-input-array)
-  (vec-extern-decl 'p_in 4 'extern-input-array)
-  (vec-extern-decl 'result_out 4 'extern-output)))
