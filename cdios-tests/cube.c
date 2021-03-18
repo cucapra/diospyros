bf152032ed40721394b3d029fc7bf98570042c3a
Jasper Liang <jxl8@cornell.edu>
2021-03-18 16:05:11 -0400

    Support for exponentiation  - expr:call has hardcoded translation between C's powf and Racket's expt functions  - new test file for cubing a vector

---
+++ cdios-tests/cube.c
@@ -0,0 +1,11 @@
+//float powf(float x, float y);
+
+#define SIZE 8
+
+void cube(float a_in[SIZE], float b_out[SIZE])
+{
+  for (int i = 0; i < SIZE; i++)
+  {
+    b_out[i] = powf(a_in[i], 3);
+  }
+}
