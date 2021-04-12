1814953ab7f803444b89adccd0ea893220ef8f36
Jonathan Tran <jtran2718@gmail.com>
2021-04-12 00:24:55 -0400

    remove compile-out

--- compile-out/spec.rkt
+++
@@ -1,5 +0,0 @@
-(list
- (box (+ (* a_in$0 b_in$0) (* a_in$1 b_in$2)))
- (box (+ (* a_in$0 b_in$1) (* a_in$1 b_in$3)))
- (box (+ (* a_in$2 b_in$0) (* a_in$3 b_in$2)))
- (box (+ (* a_in$2 b_in$1) (* a_in$3 b_in$3))))
