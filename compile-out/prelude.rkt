7ee24d83cb16036b51613a5ff1f98983d9e8af34
Jonathan Tran <jtran2718@gmail.com>
2021-04-22 17:37:48 -0400

    remove extraneous files

--- compile-out/prelude.rkt
+++
@@ -1,6 +0,0 @@
-(prog
- (list
-  (vec-const 'Z '#(0.0) 'float)
-  (vec-extern-decl 'a_in 4 'extern-input-array)
-  (vec-extern-decl 'b_in 4 'extern-input-array)
-  (vec-extern-decl 'c_out 4 'extern-output)))
