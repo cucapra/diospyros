0f63b0e7fa190d03e7133096299ae1d12da80e8e
Adrian Sampson <adrian@radbox.org>
2021-07-21 16:39:49 -0400

    Don't enable -Wall -Wextra
    These flags are enabled by default by `cc`, but they trigger lots of
    spurious warnings from the LLVM headers that we don't care about.

--- src/dios-egraphs/build.rs
+++ src/dios-egraphs/build.rs
@@ -1,14 +1,17 @@
 use std::process::Command;
 
 fn main() {
+    // Use `llvm-config` to get the appropriate flags for compiling our C++.
     let output = Command::new("llvm-config")
         .arg("--cxxflags")
         .output()
         .expect("llvm-config failed");
     let cxxflags = String::from_utf8(output.stdout).unwrap();
 
+    // Build the C++ file.
     let mut build = cc::Build::new();
     build.cpp(true)
+        .warnings(false)  // LLVM headers have lots of spurious warnings.
         .file("Diospyros/diospyros.cpp");
     for flag in cxxflags.split_ascii_whitespace() {
         build.flag(&flag);
