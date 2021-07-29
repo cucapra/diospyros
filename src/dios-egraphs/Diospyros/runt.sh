8223b796e6fea6a5fe3b8c0019228aa2f41a0f19
Jasper Liang <jxl8@cornell.edu>
2021-07-29 17:09:31 -0400

    runt script changes

--- src/dios-egraphs/Diospyros/runt.sh
+++ src/dios-egraphs/Diospyros/runt.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 args=("$@")
 FILE=target/debug/libllvmlib.so
+
+if ! [ -f $FILE ]; then
+  FILE=target/debug/libllvmlib.dylib
+fi
+
 if [[ "$OSTYPE" == "darwin"* ]]; then 
   CLANG=/usr/local/opt/llvm/bin/clang
 else
   CLANG=clang
 fi 
 
-if [ -f $FILE ]; then
-  $CLANG -Xclang -load -Xclang $FILE -emit-llvm -S -o - ${args[0]}    # .so file
-else
-  $CLANG -Xclang -load -Xclang target/debug/libllvmlib.dylib -emit-llvm -S -o - ${args[0]}    # .dylib file
-fi
+$CLANG -Xclang -load -Xclang $FILE -emit-llvm -S -o - ${args[0]} | awk '/define/{flag=1; next} /}/{flag=0} flag'   # .so file
