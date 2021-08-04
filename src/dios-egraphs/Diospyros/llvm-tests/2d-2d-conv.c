0febdf4ef4401a2607a2d64b4d2c7cbad89a8ae7
Jonathan Tran <jtran2718@gmail.com>
2021-08-04 13:35:03 -0700

    port over cdios tests

---
+++ src/dios-egraphs/Diospyros/llvm-tests/2d-2d-conv.c
@@ -0,0 +1,31 @@
+#define I_ROWS 8
+#define I_COLS 8
+#define F_ROWS 3
+#define F_COLS 3
+#define O_ROWS ((I_ROWS + F_ROWS) - 1)
+#define O_COLS ((I_COLS + F_COLS) - 1)
+
+void convolution(float mat_in[I_ROWS][I_COLS], float f_in[F_ROWS][F_COLS],
+                 float mat_out[O_ROWS][O_COLS]) {
+    for (int outRow = 0; outRow < O_ROWS; outRow++) {
+        for (int outCol = 0; outCol < O_COLS; outCol++) {
+            for (int fRow = 0; fRow < F_ROWS; fRow++) {
+                for (int fCol = 0; fCol < F_COLS; fCol++) {
+                    int fRowTrans = F_ROWS - 1 - fRow;
+                    int fColTrans = F_COLS - 1 - fCol;
+                    int iRow = outRow - fRowTrans;
+                    int iCol = outCol - fColTrans;
+
+                    if (iRow >= 0 && iRow < I_ROWS && iCol >= 0 &&
+                        iCol < I_COLS) {
+                        float v =
+                            mat_in[iRow][iCol] * f_in[fRowTrans][fColTrans];
+                        mat_out[outRow][outCol] += v;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main(void) {}
---
+++ src/dios-egraphs/Diospyros/llvm-tests/fft.c
@@ -0,0 +1,47 @@
+#define SIZE 8
+
+void fft(float real_in[SIZE], float img_in[SIZE], float real_twid_in[SIZE / 2],
+         float img_twid_in[SIZE / 2], float real_out[SIZE],
+         float img_out[SIZE]) {
+    int even = 0;
+    int odd = 0;
+    int log = 0;
+    int rootindex = 0;
+    int span = SIZE >> 1;
+    float temp = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        real_out[i] = real_in[i];
+        img_out[i] = img_in[i];
+    }
+
+    while (span != 0) {
+        odd = span;
+        while (odd < SIZE) {
+            odd = odd | span;
+            even = odd ^ span;
+
+            temp = real_out[even] + real_out[odd];
+            real_out[odd] = real_out[even] - real_out[odd];
+            real_out[even] = temp;
+
+            temp = img_out[even] + img_out[odd];
+            img_out[odd] = img_out[even] - img_out[odd];
+            img_out[even] = temp;
+
+            rootindex = (even << log) & (SIZE - 1);
+            if (rootindex > 0) {
+                temp = real_twid_in[rootindex] * real_out[odd] -
+                       img_twid_in[rootindex] * img_out[odd];
+                img_out[odd] = real_twid_in[rootindex] * img_out[odd] +
+                               img_twid_in[rootindex] * real_out[odd];
+                real_out[odd] = temp;
+            }
+            odd += 1;
+        }
+        span >>= 1;
+        log += 1;
+    }
+}
+
+int main(void) {}
