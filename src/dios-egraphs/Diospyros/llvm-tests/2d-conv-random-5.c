c98b00cee91f6ddd057127b3cfcd269454052feb
Jonathan Tran <jtran2718@gmail.com>
2021-12-14 23:17:21 -0500

    add new 1d-conv random 5 and 10 tests

--- src/dios-egraphs/Diospyros/llvm-tests/2d-conv-random-5.c
+++ src/dios-egraphs/Diospyros/llvm-tests/2d-conv-random-5.c
@@ -13,8 +13,8 @@
 #define MAX_FLOAT 100.00f
 #define DELTA 0.1f
 
-void convolution(float mat_in[I_ROWS][I_COLS], float f_in[F_ROWS][F_COLS],
-                 float mat_out[O_ROWS][O_COLS]) {
+void convolution(float mat_in[I_ROWS * I_COLS], float f_in[F_ROWS * F_COLS],
+                 float mat_out[O_ROWS * O_COLS]) {
     for (int outRow = 0; outRow < O_ROWS; outRow++) {
         for (int outCol = 0; outCol < O_COLS; outCol++) {
             for (int fRow = 0; fRow < F_ROWS; fRow++) {
@@ -26,9 +26,9 @@ void convolution(float mat_in[I_ROWS][I_COLS], float f_in[F_ROWS][F_COLS],
 
                     if (iRow >= 0 && iRow < I_ROWS && iCol >= 0 &&
                         iCol < I_COLS) {
-                        float v =
-                            mat_in[iRow][iCol] * f_in[fRowTrans][fColTrans];
-                        mat_out[outRow][outCol] += v;
+                        float v = mat_in[iRow * I_COLS + iCol] *
+                                  f_in[fRowTrans * F_COLS + fColTrans];
+                        mat_out[outRow * O_COLS + outCol] += v;
                     }
                 }
             }
@@ -39,29 +39,21 @@ void convolution(float mat_in[I_ROWS][I_COLS], float f_in[F_ROWS][F_COLS],
 int main(void) {
     time_t t = time(NULL);
     srand((unsigned)time(&t));
-    float mat_in[I_ROWS][I_COLS];
-    for (int i = 0; i < I_ROWS; i++) {
-        for (int j = 0; j < I_ROWS; j++) {
-            mat_in[i][j] = (float)rand() / (float)(RAND_MAX / MAX_FLOAT);
-        }
+    float mat_in[I_ROWS * I_COLS];
+    for (int i = 0; i < I_ROWS * I_COLS; i++) {
+        mat_in[i] = (float)rand() / (float)(RAND_MAX / MAX_FLOAT);
     }
-    float f_in[F_ROWS][F_COLS];
-    for (int i = 0; i < F_ROWS; i++) {
-        for (int j = 0; j < F_COLS; j++) {
-            f_in[i][j] = (float)rand() / (float)(RAND_MAX / MAX_FLOAT);
-        }
+    float f_in[F_ROWS * F_COLS];
+    for (int i = 0; i < F_ROWS * F_COLS; i++) {
+        f_in[i] = (float)rand() / (float)(RAND_MAX / MAX_FLOAT);
     }
-    float mat_out[O_ROWS][O_COLS];
-    for (int i = 0; i < O_ROWS; i++) {
-        for (int j = 0; j < O_COLS; j++) {
-            mat_out[i][j] = 0;
-        }
+    float mat_out[O_ROWS * O_COLS];
+    for (int i = 0; i < O_ROWS * O_COLS; i++) {
+        mat_out[i] = 0;
     }
-    float expected[O_ROWS][O_COLS];
-    for (int i = 0; i < O_ROWS; i++) {
-        for (int j = 0; j < O_COLS; j++) {
-            expected[i][j] = 0;
-        }
+    float expected[O_ROWS * O_COLS];
+    for (int i = 0; i < O_ROWS * O_COLS; i++) {
+        expected[i] = 0;
     }
     convolution(mat_in, f_in, mat_out);
     // calculate expected
@@ -76,20 +68,22 @@ int main(void) {
 
                     if (iRow >= 0 && iRow < I_ROWS && iCol >= 0 &&
                         iCol < I_COLS) {
-                        float v =
-                            mat_in[iRow][iCol] * f_in[fRowTrans][fColTrans];
-                        expected[outRow][outCol] += v;
+                        float v = mat_in[iRow * I_COLS + iCol] *
+                                  f_in[fRowTrans * F_COLS + fColTrans];
+                        expected[outRow * O_COLS + outCol] += v;
                     }
                 }
             }
         }
     }
-    for (int i = 0; i < O_ROWS; i++) {
-        for (int j = 0; j < O_COLS; j++) {
-            printf("calculated: %f\n", mat_out[i][j]);
-            printf("expected: %f\n", expected[i][j]);
-            assert(fabs(expected[i][j] - mat_out[i][j]) < DELTA);
-        }
+    for (int i = 0; i < O_ROWS * O_COLS; i++) {
+        printf("--------------------------\n");
+        printf("calculated: %f\n", mat_out[i]);
+        printf("expected: %f\n", expected[i]);
+        printf("difference: %f\n", expected[i] - mat_out[i]);
+    }
+    for (int i = 0; i < O_ROWS * O_COLS; i++) {
+        assert(fabs(expected[i] - mat_out[i]) < DELTA);
     }
     return 0;
 }
